c2e4c6017fc5f4403926a84e53a95d4973275711
codersasank <codersasank@protonmail.com>
2023-12-04 01:58:41 +0530

    Update brothers_from_different_roots.py

--- geeks_for_geeks/brothers_from_different_roots.py
+++ geeks_for_geeks/brothers_from_different_roots.py
@@ -9,16 +9,10 @@ class Solution:
         list1, list2 = list(), list()
         self.inorder(root1, list1)
         self.inorder(root2, list2)
-        dict1 = {num:True for num in list1 }
         dict2 = {num:True for num in list2 }
         cnt = 0
         for num in list1:
             if x-num in dict2:
-                del dict2[x-num]
-                print (num, x-num)
-                cnt += 1
-        for num in dict2:
-            if x-num in dict1:
-                print (num, x-num)
                 cnt += 1
         return cnt
+        
