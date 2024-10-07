fe70ef032ba12b47dfbb44772a0c07e61855cbca
codersasank <codersasank@protonmail.com>
2024-10-07 15:01:18 +0530

    Update 1431_kids_with_the_greatest.py

---
+++ leetcode/1431_kids_with_the_greatest.py
@@ -0,0 +1,9 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        max_candies = max(candies)
+        n = len(candies)
+        result = [False for i in range(n)]
+        for i in range(n):
+            if candies[i] + extraCandies >= max_candies:
+                result[i] = True
+        return result
