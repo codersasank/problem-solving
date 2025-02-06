ac25bbbdab008543eae4d2cad860c7d9c11dfa41
codersasank <codersasank@protonmail.com>
2025-02-06 21:54:29 +0530

    Update prime_partition.py

---
+++ nptel/pdsa/prime_partition.py
@@ -0,0 +1,19 @@
+import math
+def is_prime(num):
+    if num==1:
+        return False
+    for i in range(2, math.sqrt(num)+1):
+        if num%i==0:
+            return False
+    return True
+def primepartition(m):
+    primes_list = dict()
+    for i in range(2,m-1):
+        if is_prime(i):
+            primes_list[i] = True
+    for prime in primes_list:
+        if m-prime in primes_list:
+            return True
+    return False
+            
+    
