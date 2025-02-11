7ec3a8721b0692cb2fa7729859e54dbda3d086ab
codersasank <codersasank@protonmail.com>
2025-02-11 22:51:08 +0530

    Update rem_dup.py

--- nptel/pdsa/rem_dup.py
+++ nptel/pdsa/rem_dup.py
@@ -2,12 +2,10 @@ def remdup(l):
     present = dict()
     for num in l:
         present[num] = present.get(num,0) + 1
-    l.reverse()
-    idx = len(l) - 1
-    while idx >= 0:
-        num = l[idx]
-        if present[num] > 1:
-            l.pop(-1)
+    ret = list()
+    for num in l:
+        if present[num]==1:
+            ret.append(num)
+        else:
             present[num] -= 1
-        idx -= 1
-    l.reverse()
+    return ret
