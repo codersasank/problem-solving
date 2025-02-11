72a8ca29b0e48c951523220d9b3f2a3e234b443f
codersasank <codersasank@protonmail.com>
2025-02-11 23:23:46 +0530

    Update rain_average.py

--- nptel/pdsa/rain_average.py
+++ nptel/pdsa/rain_average.py
@@ -7,5 +7,6 @@ def rainaverage(l):
     ret = list()
     for city in rain_dict:
         ret.append( (city, rain_dict[city][0]/rain_dict[city][1]) )
+    ret.sort(key=lambda x:x[0])
     return ret
         
